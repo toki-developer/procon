0ce192895a2f42917d66d99cf7e89230d2568eaf
Toki <55911995+tokio-k@users.noreply.github.com>
2021-02-24 22:12:52 +0900

    add ans

---
+++ AtCoder/ABC/ABC081/B.java
@@ -0,0 +1,34 @@
+package AtCoder.ABC.ABC081;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] N = new int[n];
+        int count = 0;
+        boolean check = true;
+
+        for(int i = 0 ; i < n ; i++){
+           N[i] = sc.nextInt();
+        }
+
+        while(check){
+
+        for(int t : N ){
+           if(t % 2 == 1){
+             check = false;
+             count --;
+             break;
+           }
+        }
+        for(int i = 0 ; i < n ; i++) {
+        	N[i] /= 2;
+        }
+        count ++;
+        }
+      System.out.println(count);
+    }
+
+}
