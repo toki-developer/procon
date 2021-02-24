614c0382818d50b151c3ad3f67d37d309154ea40
Toki <55911995+tokio-k@users.noreply.github.com>
2021-02-24 21:43:17 +0900

    add ans

---
+++ AtCoder/ABC/ABC081/A.java
@@ -0,0 +1,20 @@
+package AtCoder.ABC.ABC081;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s1 = Integer.parseInt(sc.next());
+
+        int count = 0;
+        while(s1 > 0) {
+            if(s1 % 10 == 1) {
+                count ++;
+            }
+            s1 /= 10;
+        }
+        System.out.println(count);
+
+    }
+}
