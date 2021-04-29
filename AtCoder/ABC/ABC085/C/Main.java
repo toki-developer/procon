32fccee542bd87a088a014ebc4ebe2d849b47865
Toki <55911995+tokio-k@users.noreply.github.com>
2021-04-29 10:25:43 +0900

    add/ans

--- AtCoder/ABC/ABC085/C/Main.java
+++ AtCoder/ABC/ABC085/C/Main.java
@@ -7,22 +7,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.next());
         int Y = Integer.parseInt(sc.next());
-        int m1 = -1;
-        int m2 = -1;
-        int m3 = -1;
 
-        loop: for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             for (int j = 0; j <= N - i; j++) {
-                for (int k = 0; k <= N - i - j; k++) {
-                    if (Y == i * 10000 + j * 5000 + k * 1000 && i + j + k == N) {
-                        m1 = i;
-                        m2 = j;
-                        m3 = k;
-                        break loop;
-                    }
+                int k = N - i - j;
+                if (Y == i * 10000 + j * 5000 + k * 1000) {
+                    System.out.println(i + " " + j + " " + k);
+                    return;
                 }
             }
         }
-        System.out.println(m1 + " " + m2 + " " + m3);
+        System.out.println("-1 -1 -1");
     }
 }
