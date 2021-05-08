538735701d1147471540d76f5eb81d3c07c9d27c
Toki <55911995+tokio-k@users.noreply.github.com>
2021-05-08 14:14:24 +0900

    add/ans

---
+++ AtCoder/ABC/ABC196/A/Main.java
@@ -0,0 +1,14 @@
+package AtCoder.ABC.ABC196.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        int c = Integer.parseInt(sc.next());
+        int d = Integer.parseInt(sc.next());
+        System.out.println(b - c);
+    }
+}
