476c2d0c2d4798db798efa0d9117660b7b54a971
Toki <55911995+tokio-k@users.noreply.github.com>
2021-10-08 22:44:05 +0900

    add ans

---
+++ AtCoder/ABC/ABC202/A/Main.java
@@ -0,0 +1,13 @@
+package AtCoder.ABC.ABC202.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        int c = Integer.parseInt(sc.next());
+        System.out.println(21 - (a + b + c));
+    }
+}
