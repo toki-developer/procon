045b4a5784a4a622e85075f1b3dc864cf97353d3
Toki <55911995+tokio-k@users.noreply.github.com>
2021-10-26 11:59:34 +0900

    add ans

---
+++ AtCoder/ABC/ABC204/C/Main.java
@@ -0,0 +1,12 @@
+package AtCoder.ABC.ABC204.C;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.next());
+        int M = Integer.parseInt(sc.next());
+        
+    }
+}
